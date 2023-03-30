289ce28e7523d8c32aa172c6055805eb5df3cfa2
acensito <acenforos@gmail.com>
2023-03-30 19:34:54 +0200

    first commit

---
+++ app/Notifications/NewPost.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Notifications;
+
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use Illuminate\Support\Str;
+use App\Models\Post;
+
+class NewPost extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(public Post $post)
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->subject("Nuevo post de {$this->post->user->name}")
+                    ->greeting("Nuevo post de {$this->post->user->name}")
+                    ->line(Str::limit($this->post->message, 50))
+                    ->action('Ir a los posts', url('/posts'))
+                    ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
